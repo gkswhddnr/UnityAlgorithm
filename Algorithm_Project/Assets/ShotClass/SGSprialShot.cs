bf85e3beac4103159d6c4eca14a9052261d4b923
junhee4613 <junhee000123@naver.com>
2023-09-20 14:16:56 +0900

    Feat: "슈팅 매니저 추가 후 기본 샷 설정"
    스파이얼 샷 추가, 메니저 관리

---
+++ Algorithm_Project/Assets/ShotClass/SGSprialShot.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;                //추가 using 사용
+
+public class SGSprialShot : SGBaseShot
+{
+    public float startAngle = 180f;          //시작 각도
+    public float shiftAngle = 10f;           //움직이는 각도
+    public float betweenDealy = 0.2f;       
+    public int nowIndex;
+    public float delayTimer;
+
+    public override void Shot()             //Shot 필수 구현 함수(SGBaseShot)
+    {
+        if(projectileNum <= 0 || projectileSpeed <= 0f)  //옵션값검사
+        {
+            return;
+        }
+
+        if(_shooting)
+        {
+            return;
+        }
+
+        _shooting = true;
+        nowIndex = 0;
+        delayTimer = 0;
+    }
+
+      protected virtual void Update()
+    {
+        if (_shooting == false)
+        {
+            return;
+        }
+
+        delayTimer -= SGTimer.Instance.deltaTime;
+
+        while (delayTimer <= 0)
+        {
+            SGProjectile projectile = GetProjectile(transform.position);
+            if (projectile == null)
+            {
+                FinishedShot();
+                return;
+            }
+
+            float angle = startAngle + (shiftAngle * nowIndex);
+            ShotProjectile(projectile, projectileSpeed, angle);
+            projectile.UpdateMove(-delayTimer);
+
+            nowIndex++;
+
+            FiredShot();
+
+            if (nowIndex >= projectileNum)
+            {
+                FinishedShot();
+                return;
+            }
+
+            delayTimer += betweenDealy;
+
+        }
+    }
+
+
+   
+}
